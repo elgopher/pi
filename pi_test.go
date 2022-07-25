7d00021056f88e92509fcd19a99b5aee12855b44
Jacek Olszak <jacekolszak@gmail.com>
2022-07-25 20:18:50 +0200

    Add tests checking if sprite-sheet.png is loaded during Boot

--- pi_test.go
+++ pi_test.go
@@ -6,6 +6,7 @@ package pi_test
 import (
 	"strconv"
 	"testing"
+	"testing/fstest"
 
 	"github.com/elgopher/pi"
 	"github.com/elgopher/pi/image"
@@ -77,6 +78,22 @@ func TestBoot(t *testing.T) {
 		expectedSpriteSheetData := make([]byte, 16*8)
 		assert.Equal(t, expectedSpriteSheetData, pi.SpriteSheetData)
 		assert.Equal(t, allBlacks, pi.Palette)
+	})
+
+	t.Run("should load sprite-sheet.png", func(t *testing.T) {
+		pi.Reset()
+		pi.Resources = fstest.MapFS{
+			"sprite-sheet.png": &fstest.MapFile{Data: spriteSheet16x16},
+		}
+		// when
+		err := pi.Boot()
+		// then
+		require.NoError(t, err)
+		assert.Equal(t, 16, pi.SpriteSheetWidth)
+		assert.Equal(t, 16, pi.SpriteSheetHeight)
+		img := decodePNG(t, "internal/testimage/sprite-sheet-16x16.png")
+		assert.Equal(t, img.Pixels, pi.SpriteSheetData)
+		assert.Equal(t, img.Palette, pi.Palette)
 	})
 }
 
