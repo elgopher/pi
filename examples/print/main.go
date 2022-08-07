cc9c31f6f2607c19f24cd7d67ff30f33baff4694
Jacek Olszak <jacekolszak@gmail.com>
2022-08-07 17:56:53 +0200

    [docs] update comment in print example
    pi.Cursor updates the cursor position

--- examples/print/main.go
+++ examples/print/main.go
@@ -9,7 +9,7 @@ func main() {
 	pi.Draw = func() {
 		pi.CursorReset()   // set cursor to 0,0
 		pi.Color(9)        // change to yellow
-		pi.Cursor(50, 58)  // move cursor position
+		pi.Cursor(50, 58)  // set cursor position
 		pi.Print("HELLO,") // print text and go to next line
 		pi.Color(12)
 		pi.Print("GOPHER!")
