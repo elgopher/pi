db75712e2b618dcf57d0e81da1e589a526814ab5
Jacek Olszak <jacekolszak@gmail.com>
2022-11-10 19:49:22 +0100

    Add MinInt,MaxInt and MidInt functions
    These functions calculate minimum, maximum and middle numbers from integer parameters. They work will all integer parameters such as byte, int, uint etc.

---
+++ internal/fuzz/math_test.go
@@ -0,0 +1,30 @@
+// (c) 2022 Jacek Olszak
+// This code is licensed under MIT license (see LICENSE for details)
+
+//go:build !js
+
+package fuzz_test
+
+import (
+	"testing"
+
+	"github.com/elgopher/pi"
+)
+
+func FuzzMinInt(f *testing.F) {
+	f.Fuzz(func(t *testing.T, x, y int) {
+		pi.MinInt(x, y)
+	})
+}
+
+func FuzzMaxInt(f *testing.F) {
+	f.Fuzz(func(t *testing.T, x, y int) {
+		pi.MaxInt(x, y)
+	})
+}
+
+func FuzzMidInt(f *testing.F) {
+	f.Fuzz(func(t *testing.T, x, y, z int) {
+		pi.MidInt(x, y, z)
+	})
+}
