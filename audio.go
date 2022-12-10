0c5ba31286efb5334faca7ec473a77ec51dbf0ce
Jacek Olszak <jacekolszak@gmail.com>
2022-12-10 14:09:42 +0100

    Implement Audio System
    * Add ability to manage sound effects
    * Add methods for play the sound effects

--- audio.go
+++ audio.go
@@ -3,15 +3,67 @@
 
 package pi
 
+import "sync"
+
+const channelCount = 4
+const maxChannelNo = channelCount - 1
+
 // Audio returns AudioSystem object.
 func Audio() *AudioSystem {
 	return audio
 }
 
-var audio = &AudioSystem{}
+var audio = &AudioSystem{
+	effects: make([]SoundEffect, 256),
+}
 
+// AudioSystem provides methods for generating audio stream.
+// AudioSystem contains 256 sound effects, starting at 0, ending at 255.
+// AudioSystem has 4 channels. Each channel is able to play one sound effect
+// at a time.
+//
 // AudioSystem is safe to use in a concurrent manner.
-type AudioSystem struct{}
+type AudioSystem struct {
+	mutex         sync.Mutex
+	effects       []SoundEffect
+	effectsPlayed [4]effectPlayed
+}
+
+type effectPlayed struct {
+	playing     bool
+	effect      byte
+	currentNote byte
+	noteStart   byte
+	noteEnd     byte
+}
+
+// Set sets the SoundEffect with given number.
+func (s *AudioSystem) Set(number byte, e SoundEffect) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.effects[number] = e
+}
+
+// Get returns a copy of SoundEffect with given number. Modifying the returned SoundEffect
+// will not alter AudioSystem. After making changes you must run [AudioSystem.Set] to update
+// the sound effect.
+func (s *AudioSystem) Get(number byte) SoundEffect {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	return s.effects[number]
+}
+
+type SoundEffect struct {
+	Speed byte // 1 is the fastest (~8.33ms). For 120 the length of one note becomes 1 second. 0 means SoundEffect takes no time.
+	Notes [32]Note
+}
+
+type Note struct {
+	Pitch  byte
+	Volume byte // 0 - quiet. 255 - loudest. 255 values is way too much!
+}
 
 // Read method is used by back-end to read generated audio stream and play it back to the user. The sample rate is 44100,
 // 16 bit depth and stereo (2 audio channels).
@@ -23,10 +75,51 @@ func (s *AudioSystem) Read(p []byte) (n int, err error) {
 		return 0, nil
 	}
 
-	// generate silence for now
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	for i := 0; i < len(p); i++ {
 		p[i] = 0
 	}
 
+	for _, e := range s.effectsPlayed {
+		if !e.playing {
+			continue
+		}
+
+		for i := 0; i < len(p); i++ {
+			p[i] = byte(i)
+		}
+	}
+
 	return len(p), nil
+}
+
+func (s *AudioSystem) Play(soundEffect, channelNo, noteStart, noteEnd byte) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if channelNo > maxChannelNo {
+		return
+	}
+
+	s.effectsPlayed[channelNo] = effectPlayed{
+		playing:     true,
+		effect:      soundEffect,
+		currentNote: 0,
+		noteStart:   0,
+		noteEnd:     0,
+	}
+}
+
+func (s *AudioSystem) Reset() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.effects = make([]SoundEffect, 256)
+	s.effectsPlayed = [4]effectPlayed{}
+}
+
+func Sfx(soundEffect byte) {
+	audio.Play(soundEffect, 0, 0, 31)
 }
--- audio_test.go
+++ audio_test.go
@@ -4,6 +4,7 @@
 package pi_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,5 +27,75 @@ func TestAudioSystem_Read(t *testing.T) {
 		require.NoError(t, err)
 		expected := make([]byte, n)
 		assert.Equal(t, expected, buffer)
+	})
+}
+
+func TestAudio(t *testing.T) {
+	t.Run("should not data race when run with -race flag", func(t *testing.T) {
+		audio := pi.Audio()
+		const goroutineCount = 100
+		var wg sync.WaitGroup
+		wg.Add(goroutineCount)
+		for i := 0; i < goroutineCount; i++ {
+			go func() {
+				audio.Set(0, pi.SoundEffect{})
+				_ = audio.Get(0)
+				audio.Play(0, 0, 0, 0)
+				_, err := audio.Read(make([]byte, 8192))
+				require.NoError(t, err)
+				audio.Reset()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	})
+}
+
+func TestAudioSystem_Set(t *testing.T) {
+	t.Run("should set sound effect", func(t *testing.T) {
+		given := pi.SoundEffect{Speed: 1}
+		given.Notes[0] = pi.Note{Pitch: 1, Volume: 2}
+		given.Notes[31] = pi.Note{Pitch: 3, Volume: 4}
+		// when
+		pi.Audio().Set(0, given)
+		// then
+		actual := pi.Audio().Get(0)
+		assert.Equal(t, given, actual)
+		assert.NotSame(t, given.Notes, actual.Notes)
+	})
+}
+
+func TestAudioSystem_Play(t *testing.T) {
+	effect := pi.SoundEffect{
+		Speed: 1, // speed 1 is ~8.333 ms
+		Notes: [32]pi.Note{
+			{Pitch: 127, Volume: 255},
+		},
+	}
+
+	t.Run("should generate audio stream", func(t *testing.T) {
+		audio := pi.Audio()
+		audio.Reset()
+		audio.Set(0, effect)
+		// when
+		audio.Play(0, 0, 0, 1)
+		// then
+		buffer := make([]byte, 1500)
+		_, err := audio.Read(buffer)
+		require.NoError(t, err)
+		assert.NotEqual(t, make([]byte, 1500), buffer)
+	})
+
+	t.Run("should not generate audio stream when channel is higher than max", func(t *testing.T) {
+		audio := pi.Audio()
+		audio.Reset()
+		audio.Set(0, effect)
+		// when
+		audio.Play(0, 4, 0, 1)
+		// then
+		buffer := make([]byte, 1500)
+		_, err := audio.Read(buffer)
+		require.NoError(t, err)
+		assert.Equal(t, make([]byte, 1500), buffer)
 	})
 }
