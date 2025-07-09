38278f9c3d7182e7c54db62ac4c8a4dcfc5bacbe
Jacek Olszak <jacekolszak@gmail.com>
2025-07-09 16:29:27 +0200

    Fix typo in EventLateUpdate const

--- piloop/event.go
+++ piloop/event.go
@@ -9,7 +9,7 @@ const (
 	EventGameStarted  Event = "game_started"  // just before first frame
 	EventFrameStarted Event = "frame_started" // beginning of the frame
 	EventUpdate       Event = "update"        // after pi.Update
-	EventLateUpdate   Event = "late_draw"     // after EventUpdate
+	EventLateUpdate   Event = "late_update"   // after EventUpdate
 	EventDraw         Event = "draw"          // after pi.Draw
 	EventLateDraw     Event = "late_draw"     // after EventDraw
 	EventWindowClosed Event = "window_closed" // when user closes the window (desktop only)
