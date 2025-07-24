f6264d8845bddd97dd813e8ded8958fa92039044
Jacek Olszak <jacekolszak@gmail.com>
2025-07-25 00:29:59 +0200

    test: piebiten.CopyCanvasToEbitenImage
    Add basic integration tests for piebiten.CopyCanvasToEbitenImage

--- palettemap.go
+++ palettemap.go
@@ -14,7 +14,7 @@ import (
 // the contents of the Screen to the game window.
 //
 // The array index is the original color, and the value is the mapped color.
-var PaletteMapping PaletteMap = notSwappedPaletteMapping
+var PaletteMapping PaletteMap = notRemappedPalette
 
 // PaletteMap defines the color mapping for display.
 //
@@ -32,10 +32,10 @@ func (p PaletteMap) String() string {
 }
 
 func ResetPaletteMapping() {
-	PaletteMapping = notSwappedPaletteMapping
+	PaletteMapping = notRemappedPalette
 }
 
-var notSwappedPaletteMapping = func() PaletteMap {
+var notRemappedPalette = func() PaletteMap {
 	var m PaletteMap
 	for i := 0; i < MaxColors; i++ {
 		m[i] = Color(i)
