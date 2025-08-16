8c9ecb320f3c5ee94166680d32b5ae4e376517fd
Jacek Olszak <jacekolszak@gmail.com>
2025-08-16 19:57:14 +0200

    fix: panic when resuming empty Routine

--- piroutine/piroutine_test.go
+++ piroutine/piroutine_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/elgopher/pi/piroutine"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPiroutine(t *testing.T) {
@@ -18,4 +19,29 @@ func TestPiroutine(t *testing.T) {
 	r.SetTracing(true)
 	for r.Resume() {
 	}
+}
+
+func TestRoutine_Resume(t *testing.T) {
+	t.Run("empty routine", func(t *testing.T) {
+		routine := piroutine.New()
+		assert.False(t, routine.Resume())
+	})
+
+	t.Run("single step", func(t *testing.T) {
+		var stepExecuted bool
+		step := func() bool {
+			stepExecuted = true
+			return true // finish immediately
+		}
+		routine := piroutine.New(step)
+		assert.False(t, routine.Resume()) // finished
+		assert.True(t, stepExecuted)
+	})
+
+	t.Run("wait step", func(t *testing.T) {
+		routine := piroutine.New(piroutine.Wait(1))
+		assert.True(t, routine.Resume())  // not yet finished
+		assert.False(t, routine.Resume()) // finished
+		assert.False(t, routine.Resume()) // nothing changed
+	})
 }
